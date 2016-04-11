d4cc224fcb7f7d6b614a3ac8f07cdae15bf170c6
bevieboo <bevin.sng@gmail.com>
2016-04-11 19:33:58 +1000

    remove some more comments

--- app.rb
+++ app.rb
@@ -9,23 +9,36 @@ end
 
 get '/movie_list' do
   if params[:movie] != nil
-    movie_name = params[:movie].to_s
+    movie_name = params[:movie]
     @result = HTTParty.get("http://omdbapi.com/?s=#{movie_name}")
-      # @result.inspect
-      if @result['Search'].length == 1
-        redirect "/movie_info?imdbID=#{ @result['Search'][0]['imdbID'] }"
-      end
-
+    if @result['Search'].length == 1
+      redirect "/movie_info?imdbID=#{ @result['Search'][0]['imdbID'] }"
+    end
   end
   erb :movie_list
 end
 
 get '/movie_info' do
-
     movie_id = params[:imdbID]
     @movie_info = HTTParty.get("http://omdbapi.com/?i=#{movie_id}")
-
   erb :movie_info
 end
 
 # binding.pry
+
+# get '/movie_info' do
+#   if params[:movie] != nil
+#     movie_name = params[:movie].to_s
+#     @result = HTTParty.get("http://omdbapi.com/?s=#{movie_name}")
+#
+#     if @result['Search'].length == 1
+#       url = 'http://omdbapi.com/?t='+movie_name
+#       @movie_info = HTTParty.get(url)
+#
+#     elsif @result['Search'].length > 1
+#       redirect "/movie_list"
+#     end
+#
+#   end
+#   erb :movie_info
+# end
